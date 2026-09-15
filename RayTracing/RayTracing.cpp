08ec825f7f71f9d3ac3302e64cf9055af4e116f8
刘金昭 <953958973@qq.com>
2026-09-15 17:20:25 +0800

    Initial commit: ray tracing project

---
+++ RayTracing/RayTracing.cpp
@@ -0,0 +1,32 @@
+﻿// RayTracing.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+
+int main()
+{
+	 // Image
+	int image_width{256};
+	int image_height{256};
+
+	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j{ 0 }; j < image_height; ++j) {
+		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+		for (int i{ 0 }; i < image_width; ++i) {
+			auto r{ static_cast<double>(i) / (image_width - 1) };
+			auto g{ static_cast<double>(j) / (image_height - 1) };
+			auto b{ 0.0 };
+
+			int ir{ static_cast<int>(255.999 * r)  };
+			int ig{ static_cast<int>(255.999 * g) };
+			int ib{ static_cast<int>(255.999 * b) };
+
+			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+
+		}
+	}
+
+	std::clog << "\rDone.                 \n";
+}
---
+++ RayTracing/vec3.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+
+class vec3
+{
+public:
+	double e[3];
+
+	vec3() :e{ 0,0,0 } {}
+	vec3(double e0, double e1, double e2) : e{ e0,e1,e2 } {}
+
+	// getter
+	double x() const { return e[0]; }
+	double y() const { return e[1]; }
+	double z() const { return e[2]; }
+
+	// 运算符重载
+	vec3 operator- () const { return vec3(-e[0], -e[1], -e[2]); }
+	double operator[] (int i) const { return e[i]; }
+	double& operator[](int i) { return e[i]; }
+
+	vec3& operator+=(const vec3& v) {
+		e[0] += v.e[0];
+		e[1] += v.e[1];
+		e[2] += v.e[2];
+		return *this;
+	}
+
+	vec3& operator*=(double t) {
+		e[0] *= t;
+		e[1] *= t;
+		e[2] *= t;
+		return *this;
+	}
+
+	vec3& operator/=(double t) {
+		return *this *= 1 / t;
+	}
+
+	//  获取向量长度
+	double length() const {
+		return std::sqrt(length_squared());
+	}
+
+	double length_squared() const {
+		return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
+	}
+};
+
+
+// alias
+using point3 = vec3;
+
+// 向量工具方法
+
+inline std::ostream& operator<< (std::ostream& out, const vec3& v) {
+	return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+}
+
+
+// 向量加法
+inline vec3 operator+(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+}
+
+// 向量减法
+inline vec3 operator-(const vec3& u, const vec3& v) {
+	return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+}
+
+// 乘法
+inline vec3 operator*(double t, const vec3& v) {
+	return vec3(t * v.e[0], t * v.e[1], t * v.e[2]);
+}
+
+inline vec3 operator*(const vec3& v, double t) {
+	return t * v;
+}
+
+
+// 除法
+inline vec3 operator/(const vec3& v, double t) {
+	return (1 / t) * v;
+}
+
+// 点积
+inline double dot(const vec3& u, const vec3& v) {
+	return u.e[0] * v.e[0]
+		+ u.e[1] * v.e[1]
+		+ u.e[2] * v.e[2];
+}
+
+// 叉积
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+		u.e[2] * v.e[0] - u.e[0] * v.e[2],
+		u.e[0] * v.e[1] - u.e[1] * v.e[0]);
+}
+
+// 单位化
+inline vec3 unit_vector(const vec3& v) {
+	return v / v.length();
+}
