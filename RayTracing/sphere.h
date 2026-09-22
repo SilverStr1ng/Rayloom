e81140f73d2f9d628132cdee63b59a926552b1cf
刘金昭 <953958973@qq.com>
2026-09-22 17:53:02 +0800

    feat(material): 添加材质类及其子类以支持光线散射
    添加了材质类及其子类（lambertian、metal、dielectric），
    以实现不同材质的光线散射效果。更新了相关的球体和
    世界对象以使用这些材质。

--- RayTracing/sphere.h
+++ RayTracing/sphere.h
@@ -5,8 +5,8 @@
 
 class sphere : public hittable {
 public:
-	sphere(const point3& center, double radius)
-		: center{ center }, radius{ std::fmax(0, radius) } {
+	sphere(const point3& center, double radius, std::shared_ptr<material> mat)
+		: center{ center }, radius{ std::fmax(0, radius) }, mat{ mat } {
 
 	}
 
@@ -38,6 +38,7 @@ public:
 		rec.p = r.at(rec.t);
 		vec3 outward_normal{ (rec.p - center) / radius };
 		rec.set_face_normal(r, outward_normal);
+		rec.mat = mat;
 
 		return true;
 	}
@@ -45,4 +46,5 @@ public:
 private:
 	point3 center;
 	double radius;
+	std::shared_ptr<material> mat;
 };
